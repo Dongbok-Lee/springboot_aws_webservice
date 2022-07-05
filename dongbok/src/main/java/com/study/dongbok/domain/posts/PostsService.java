6591472120110f05adbc4c92e33585f62a23cce0
동복 <typoon0820@gmail.com>
2022-07-05 20:28:06 +0900

    ~page162 complete

--- dongbok/src/main/java/com/study/dongbok/domain/posts/PostsService.java
+++ dongbok/src/main/java/com/study/dongbok/domain/posts/PostsService.java
@@ -1,9 +1,15 @@
 package com.study.dongbok.domain.posts;
 
+import com.study.dongbok.web.dto.PostsListResponseDto;
+import com.study.dongbok.web.dto.PostsResponseDto;
 import com.study.dongbok.web.dto.PostsSaveRequestDto;
+import com.study.dongbok.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -13,5 +19,38 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new
+                        IllegalArgumentException("해당게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(),requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(()-> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+
+        postsRepository.delete(posts);
     }
 }
--- dongbok/src/main/java/com/study/dongbok/web/PostsApiController.java
+++ dongbok/src/main/java/com/study/dongbok/web/PostsApiController.java
@@ -1,11 +1,12 @@
 package com.study.dongbok.web;
 
 import com.study.dongbok.domain.posts.PostsService;
+import com.study.dongbok.web.dto.PostsResponseDto;
 import com.study.dongbok.web.dto.PostsSaveRequestDto;
+import com.study.dongbok.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,4 +18,21 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+
 }
